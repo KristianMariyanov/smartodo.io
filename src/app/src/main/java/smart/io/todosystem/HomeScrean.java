8340e17215e69b905a62b4a702c2504dd8b4d7b4
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-12-23 20:48:27 +0200

    Added initial project

---
+++ src/app/src/main/java/smart/io/todosystem/HomeScrean.java
@@ -0,0 +1,13 @@
+package smart.io.todosystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeScrean extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screan);
+    }
+}
